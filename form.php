1d21ce8ad71a79941e3fe46374a8a663e2115f3c
Shokunbi Johnson <maximusjshokes@gmail.com>
2020-10-12 23:34:04 +0100

    Initial commit

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="http://butopia.com.ng/app/trial_request/" method="POST">
+        <input type="text" name="email" placeholder="email">
+        <input type="text" name="pseudoId" placeholder="pseudoID">
+        <input type="text" name="deviceInformation" placeholder="Device Information">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+
+</html>
